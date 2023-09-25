cb17e5aadb8801cd42c2312a1cac00fea8e9e6d9
Giordano Lipari @laptop4 <wmotion.eu@gmail.com>
2023-09-25 10:25:17 +0200

    gitautosync update 12

--- script.R
+++ script.R
@@ -3,4 +3,4 @@ read.csv(file = "data/inflammation-01.csv", header = TRUE)
 # read.csv(file = "data/commedec.txt")
 read.csv(file = "data/commadec.txt", sep = ';', dec=',')
 # variables
-weight_kg
+weight_kg <- 55
