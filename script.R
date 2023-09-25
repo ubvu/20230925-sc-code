ed8e2c35a31f9aa58bcdc3dac3ad698afd9ad6d2
Giordano Lipari @laptop4 <wmotion.eu@gmail.com>
2023-09-25 15:12:13 +0200

    gitautosync update 19

--- script.R
+++ script.R
@@ -1,5 +1,5 @@
-read.csv(file = "data/inflammation-01.csv", header = FALSE)
 read.csv(file = "data/inflammation-01.csv", header = TRUE)
+read.csv(file = "data/inflammation-01.csv", header = FALSE)
 # read.csv(file = "data/commedec.txt")
 read.csv(file = "data/commadec.txt", sep = ';', dec=',')
 # variables
