b1c46c16ea289d158ce86e63f6f6bd63b92534bb
Giordano Lipari @laptop4 <wmotion.eu@gmail.com>
2023-09-25 10:15:16 +0200

    gitautosync update 7

--- script.R
+++ script.R
@@ -1,3 +1,4 @@
 read.csv(file = "data/inflammation-01.csv", header = FALSE)
 read.csv(file = "data/inflammation-01.csv", header = TRUE)
 read.csv(file = "data/commedec.txt")
+read.csv(file = "data/commadec.txt")
