603cf103d327cb5afd237a1bb84a9c5888b8cab3
tijs bliek <bliek@uva.nl>
2023-09-26 11:52:15 +0200

    gitautosync update 9

---
+++ Conditionals.r
@@ -0,0 +1,16 @@
+## funtion to plot data from an inflammation file
+
+analyze <- function(filename) {
+  # Plots the average, min, and max inflammation over time.
+  # Input is character string of a csv file.
+  dat <- read.csv(file = filename, header = FALSE)
+  avg_day_inflammation <- apply(dat, 2, mean)
+  plot(avg_day_inflammation)
+  max_day_inflammation <- apply(dat, 2, max)
+  plot(max_day_inflammation)
+  min_day_inflammation <- apply(dat, 2, min)
+  plot(min_day_inflammation)
+}
+
+## run the function on inflammation_01.csv
+analyze("data/inflammation-1.csv")
