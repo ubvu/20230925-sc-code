10e4ede9967a7358ffdf4cb72abfa87cdddf57ad
tijs bliek <bliek@uva.nl>
2023-09-26 13:36:08 +0200

    gitautosync update 16

--- Conditionals.r
+++ Conditionals.r
@@ -24,3 +24,64 @@ for (f in file_list) {
   analyze(f)
   print(f)
 }
+
+
+
+## plots to pdf
+pdf("results/inflammation-01.pdf")
+analyze("data/inflammation-01.csv")
+dev.off()
+
+
+output_list <- gsub("csv", "pdf", file_list)
+output_fullnames <- gsub("data", "results", output_list)
+
+for (f in file_list) {
+  f2 <- gsub("csv", "pdf", f)
+  f3 <- gsub("data", "results", f2)
+  print(f3)
+  pdf(f3)
+  analyze(f)
+  print("this is a plot")
+  dev.off()
+  }
+
+num <- 37
+
+if (num < 100) {
+  print("smaller than 100")
+} else {
+  print("great")
+}
+
+
+sign <- function(num) {
+  if (num > 0) {
+    return(1)
+  } else if (num == 0) {
+    return(0)
+  } else if (num < 0) {
+    return(-1)
+  }
+}
+
+sign(0)
+
+
+###   >= greater than or equal
+###   <= smaller than or equal
+
+num <- 37
+
+if (num > 0 && num > 50) {
+  print("both are true")
+} else {
+  print("At least one of the two is not true")
+}
+  
+if (num > 0 || num > 50) {
+  print("at least one is true")
+}  else {
+  print("neither part is true")
+}
+
